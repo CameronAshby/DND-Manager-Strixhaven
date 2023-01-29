f207015ce630290c38de707040752747a8154a9f
CameronAshby <c00ashby@gmail.com>
2023-01-29 14:33:50 -0700

    updated inventory quantity buttons to call corresponding functions and refactored building into a for loop

--- Pages/inventory.js
+++ Pages/inventory.js
@@ -2,19 +2,25 @@ import itemData from './inventoryData.json' assert { type: "json"};
 
 function buildInventory() {
     var bagOfHoldingItems = itemData.bag_of_holding.items;
-    bagOfHoldingItems.forEach((item) => {
+
+    for(let i=0; i<bagOfHoldingItems.length; i++) {
         $('.bagItems').append(
             `<div class='item'>
-                <div class='itemName'>`+item.name+`</div>
-                <div class='itemDescription'>`+item.description+`</div>
+                <div id=itemName`+bagOfHoldingItems[i].id+` class='itemName'>`+bagOfHoldingItems[i].name+`</div>
+                <div id=itemDescription`+bagOfHoldingItems[i].id+` class='itemDescription'>`+bagOfHoldingItems[i].description+`</div>
                 <div class='itemQuantity'>
-                    <button type='button' class='addButton'>+</button>
-                    `+'&nbsp&nbsp'+item.quantity+'&nbsp&nbsp'+`
-                    <button type='button' class='minusButton'>-</button>
+                    <button id=addButton`+bagOfHoldingItems[i].id+` type='button' class='addButton'>+</button>
+                    `+'&nbsp&nbsp'+bagOfHoldingItems[i].quantity+'&nbsp&nbsp'+`
+                    <button id=minusButton`+bagOfHoldingItems[i].id+` type='button' class='minusButton'>-</button>
                 </div>
             </div>`
         );
-    })
+    }
+
+    for(let i=0; i<bagOfHoldingItems.length; i++) {
+        $('#addButton'+[i+1]).on('click',addQuantity);
+        $('#minusButton'+[i+1]).on('click',minusQuantity);
+    }
 }
 
 function addItem() {
@@ -23,6 +29,14 @@ function addItem() {
 
 function removeItem() {
 
+}
+
+function addQuantity() {
+    console.log('adding');
+}
+
+function minusQuantity() {
+    console.log('subtracting');
 }
 
 window.onload = buildInventory;
